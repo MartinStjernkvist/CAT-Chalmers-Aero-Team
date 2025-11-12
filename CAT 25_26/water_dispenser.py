1fe86fb34d211a3d6f9eea4021808956e372e4b7
MartinStjernkvist <mr.martin.pro@gmail.com>
2025-11-12 20:28:59 +0100

    .

--- CAT 25_26/water_dispenser.py
+++ CAT 25_26/water_dispenser.py
@@ -1,29 +1,29 @@
 #%%
 import numpy as np
 
-V = 4 * 10**(-3) # m^3
-h = 0.15 # m
+V = 4 * 10**(-3) # volume of tank in m^3
+h = 0.15 # height of tank in meters
 
-# A_tank = V / h
+A_tank = V / h # area of tank, box shape
 
-g = 9.81
+g = 9.81 # grativy constant
 
-distance = 150
-velocity = 15
+distance = 150 # double-check
+velocity = 15 # cruise
 
-t = distance / velocity
+t = distance / velocity # time between waypoints
 
-Cd = 0.9
+Cd = 0.9 # discharge coefficient
 
-def d(V, h, t):
-    # A_outlet = A_tank / (g * t) * np.sqrt(2 * g * h)
-    A_outlet = 2 * V / (Cd * t * np.sqrt(2 * g * h)) 
+def diameter(A_tank, h, t):
+    A_outlet = A_tank / (Cd * g * t) * np.sqrt(2 * g * h)
+    # A_outlet = 2 * V / (Cd * t * np.sqrt(2 * g * h)) 
     r = np.sqrt(A_outlet / np.pi)
     d = r * 2
     return d
 
-diameter = d(V, h, t)
-print('diameter', f'{diameter * 1000:.1f}', ' mm')
+d = diameter(A_tank, h, t)
+print('diameter', f'{d * 1000:.1f}', ' mm')
 #%%
 
 
