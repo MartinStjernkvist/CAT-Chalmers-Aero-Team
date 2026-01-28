0a73fc7008e95d5f754fa02e8be1954284005b70
MartinStjernkvist <mr.martin.pro@gmail.com>
2026-01-28 14:08:11 +0100

    .

--- CAT 25_26/battery_capacity.py
+++ CAT 25_26/battery_capacity.py
@@ -9,10 +9,21 @@ import numpy as np
 #===================================================================================================
 m = 10 # kg
 g = 9.82
+rho_air = 1.225 # kg/m^3
 
 # Lift
 L = m * g
 L_D_ratio = 9
+CL_max = 1.8
+CD = 0.05
+
+wingspan = 2.5
+chord = 0.345
+
+takeoff_margin = 0.2
+mu = 0.3 # grass and rubber
+l_runway = 20
+thrust_to_weight_takeoff = 0.5
 
 # Battery
 battery_cap_Ah = 6 # Ah
@@ -24,6 +35,8 @@ leftover_cap_factor = 0.2
 eta_batteries = 0.8
 eta_prop = 0.6
 eta_motor_ESC = 0.85
+eta_motor_takeoff = 4.10 # g/W, 90% throttle
+e = 0.8 # Oswald efficiency, rectangular wing
 
 # total distance 
 distance_lap = 2 * 2_800 # m
@@ -36,6 +49,49 @@ v_wind = 20 * 0.4
 #===================================================================================================
 # CALCULATIONS
 #===================================================================================================
+S = wingspan * chord
+AR = wingspan**2 / S
+
+v_stall = np.sqrt(2 * L / (rho_air * S * CL_max))
+print(f'\nstall speed: \n{v_stall:.2f} m/s')
+
+v_takeoff = (1 + takeoff_margin) * v_stall
+print(f'\ntake-off speed: \n{v_takeoff:.2f} m/s')
+
+CL_takeoff = CL_max / (1 + takeoff_margin)**2
+
+CD_induced = CL_takeoff**2 / (np.pi * AR * e)
+print(f'\ninduced drag coefficient \n{CD_induced:.2f}')
+
+CD_tot = CD + CD_induced
+print(f'\ntotal drag coefficient \n{CD_tot:.2f} N')
+
+D_takeoff = 1/2 * rho_air * v_takeoff**2 * S * CD_tot
+print(f'\ndrag during take-off: \n{D_takeoff:.2f}')
+
+F_fric = mu * L
+
+a_takeoff = v_takeoff**2 / (2 * l_runway)
+print(f'\nacceleration at take-off: \n{a_takeoff:.2f} m/s^2')
+
+F_acc_takeoff = m * a_takeoff
+print(f'\nforce from acceleration at take-off \n{F_acc_takeoff:.2f} N')
+
+T_takeoff = D_takeoff + F_fric + F_acc_takeoff
+print(f'\nthrust at take-off: \n{T_takeoff:.2f} N')
+
+# T_target = thrust_to_weight_takeoff * L
+# print(f'\nthrust at take-off (calculated with thrust-to-weight ratio): \n{T_target:.2f} N')
+
+P_out_takeoff = T_takeoff * v_takeoff
+print(f'\npower delivered to air at take-off: \n{P_out_takeoff:.2f} W')
+
+P_electrical_takeoff = P_out_takeoff / (eta_prop * eta_motor_ESC)
+print(f'\nactual electrical power at take-off: \n{P_electrical_takeoff:.2f} W')
+
+P_g_per_W =  T_takeoff * (1000 / g) / eta_motor_takeoff
+print(f'\nactual electrical power at take-off (g/W method): \n{P_g_per_W:.2f} W')
+
 D = L / L_D_ratio
 print(f'\ndrag: \n{D:.2f} N')
 
